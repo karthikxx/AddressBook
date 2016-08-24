706d5f1920e145cefd1676ed5137b790f450a050
karthikxx <karthik910@gmail.com>
2016-08-24 12:10:14 -0700

    removing the old es5 ui implementation

--- AddressBook.Services/AddressBook.React.Web/src/components/main.jsx
+++
@@ -1,8 +0,0 @@
-﻿var React = require('react');
-
-var ReactDOM = require('react-dom');
-
-var ContactBox = require('./contactBox');
-
-ReactDOM.render(<ContactBox url="http://localhost:5000/api/contacts"></ContactBox>, document.getElementById('root'));
-
